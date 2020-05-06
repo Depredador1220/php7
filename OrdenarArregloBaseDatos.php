7cd7ae1764e4e318619fe41b0007b493e0350e4e
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-05-05 19:46:02 -0500

    Create OrdenarArregloBaseDatos.php

---
+++ OrdenarArregloBaseDatos.php
@@ -0,0 +1,20 @@
+<?php
+	$con = mysqli_connect("127.0.0.1", "root", "", "samples");
+	$rs = mysqli_query($con, "select * from person");
+
+	if($rs)
+	{
+		$personas = array();
+
+		while($row = mysqli_fetch_assoc($rs))
+		{
+			$personas[] = $row;		
+		}
+
+		mysqli_free_result($rs);
+	}
+
+	mysqli_close($con);
+	sort($personas);
+	print_r($personas);
+?>
