12eea71a860fceaa6eda988b09ed90c52fcb4f23
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-05-23 15:47:07 -0500

    Create pila.php

---
+++ pila.php
@@ -0,0 +1,13 @@
+<?php
+	$pila = array();
+	array_push($pila, 'Naranja');
+	array_push($pila, 'Manzana');
+	array_push($pila, 'Platano');
+
+	print_r($pila);
+
+	echo '<br />';
+
+	$fruta = array_pop($pila);
+	print_r($pila)
+?>
